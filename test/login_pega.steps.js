d0a062ece641c7c5414ffb242892c36ca72106b7
Aliaksei Sidar <Aliaksei_Sidar@epam.com>
2021-03-19 15:47:43 +0300

    qewrty

--- test/login_pega.steps.js
+++ test/login_pega.steps.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-describe('H1 Text', async () => {
+describe('PEGA core', async () => {
   let browser
   let page
   beforeAll(async () => {
@@ -10,31 +10,52 @@ describe('H1 Text', async () => {
      page = await browser.newPage();
     page.emulate({
       viewport: {
-        width: 1024,
-        height: 2400,
+        width: 1280,
+        height: 800,
       },
       userAgent: '',
     });
   })
 
-  test('h1 loads correctly', async () => {
+  test('Success login', async () => {
     await page.goto('https://lloyds-chorus-dt3.pegacloud.net/prweb/');
     await page.waitForSelector('#credentialsForm');
-    await page.type('#txtUserID', 'dev2');
-    await page.type('#txtPassword', 'defari00');
-    await page.click('#sub');
-    await page.waitForSelector('#error');
-    const errorText = await page.$eval('#error', e => e.innerText);
-    expect(errorText).toEqual('The information you entered was not recognized.');
-      }, 60000);
-
-  test('h1 loads correctly2', async () => {
     await page.type('#txtUserID', 'as_BKFittingUG2RWS@lloyds.com');
     await page.type('#txtPassword', 'Dcom2020!');
     await page.click('#sub');
-    await page.waitForSelector('#error');
-    const errorText = await page.$eval('#error', e => e.innerText);
-    expect(errorText).toEqual('The information you entered was not recognized.');
-  }, 60000);
+    await page.waitForSelector('.layout-noheader-portal_header');
+    const headerText = await page.$eval('a.Header_nav', e => e.innerText);
+    expect(headerText).toEqual('Delegated contract manager');
+      }, 60000);
 
+  test('Agreement Template page loads', async () => {
+    await page.waitForSelector('div#workarea');
+    await page.waitForSelector('iframe');
+    const elementHandle = await page.$('iframe');
+    const frame1 = await elementHandle.contentFrame();
+    await frame1.waitForSelector('div.content-item.content-layout.item-2.flex.flex-row');
+    await frame1.waitForSelector('div.lloyds-quickstart-container-contract');
+    await frame1.click('div.lloyds-quickstart-container-contract');
+      }, 60000);
+
+  test('Agreement Template page loads', async () => {
+    await page.waitForSelector('div[title^="CON"] ~ div>iframe')
+    const elementHandle = await page.$('div[title^="CON"] ~ div>iframe');
+    const frame2 = await elementHandle.contentFrame();
+    await frame2.waitForSelector('div[node_name="pyTabbedScreenFlow7Main"]');
+    const headerText = await frame2.$eval('div[param_name="EXPANDEDSubSectionAboutContractB"]>div[node_type="HEADER"] h1', e => e.innerText);
+    expect(headerText).toEqual('Agreement template');
+      }, 60000);
+
+  test('Fill Agreement Template page', async () => {
+    const elementHandle = await page.$('div[title^="CON"] ~ div>iframe');
+    const frame2 = await elementHandle.contentFrame();
+    await frame2.waitForSelector('.content-layout.item-3');
+    await frame2.click('.content-layout.item-3 button');
+    await frame2.waitForSelector('ul#apresults-list')
+    await frame2.waitFor(1000)
+    await frame2.click('li[category-header="Binding Authority Agreement"]');
+    await frame2.waitFor(1000)
+    await frame2.click('ul#apresults-list>li:nth-child(2)');
+  }, 60000);
 });
